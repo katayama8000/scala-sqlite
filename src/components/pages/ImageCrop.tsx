8eab15a0db71c165cb528470cbacc4940faf259c
katayama8000 <tattu.0310@gmail.com>
2023-01-05 21:01:46 +0900

    reset()

--- src/components/pages/ImageCrop.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, VFC } from "react";
-import ReactDOM from "react-dom";
-import React, { PureComponent } from "react";
-import ReactCrop from "react-image-crop";
-import "react-image-crop/dist/ReactCrop.css";
-
-export const ImageCrop: VFC = memo(() => {
-  return <p>crop</p>;
-});

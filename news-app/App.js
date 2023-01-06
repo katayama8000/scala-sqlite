8f85822527a30e742c4adffd6347abaeef0fa258
katayama8000 <tattu.0310@gmail.com>
2023-01-06 18:40:46 +0900

    feat: add picture

--- news-app/App.js
+++ news-app/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>this is first</Text>
-      <View style={styles.itemContainer}>
+      <View style={styles.itemContainer} >
+        <View style={styles.leftContainer} >
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: "https://picsum.photos/id/10/200/200" }}
+          />
+        </View>
+        <View style={styles.rightContainer} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -24,5 +31,14 @@ const styles = StyleSheet.create({
     width: "100%",
     borderColor: "red",
     borderWidth: 1,
-  }
+    flexDirection: "row",
+  },
+  leftContainer: {
+    backgroundColor: "red",
+    width: 100,
+  },
+  rightContainer: {
+    backgroundColor: "blue",
+    flex: 1,
+  },
 });

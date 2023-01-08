040f36ab9a038c7188ebdd66cbfab63d1cda3552
katayama8000 <tattu.0310@gmail.com>
2023-01-08 21:02:49 +0900

    update

--- news-app/App.js
+++ news-app/App.js
@@ -1,22 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import ListItem from './components/ListItem';
+import articles from "./dummies/articles.json"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer} >
-        <View style={styles.leftContainer} >
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: "https://picsum.photos/id/10/200/200" }}
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
           />
-        </View>
-        <View style={styles.rightContainer} >
-          <Text numberOfLines={3}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-          <Text>Price: $100</Text>
-        </View>
-      </View>
-      <StatusBar style="auto" />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -25,20 +25,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    borderColor: "red",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
   },
 });

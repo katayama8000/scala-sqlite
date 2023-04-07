98785c1e0159f81dee92d7fba9ebf8349ee85617
katayama8000 <tattu.0310@gmail.com>
2023-04-07 21:34:09 +0900

    add button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   useEffect(() => {
-    alert('Hello World');
+    console.log('Hello World');
   }, []);
+  const handlePress = () => {
+    alert('Hello World');
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!!</Text>
+      <Button title="Click Me" onPress={handlePress} />
     </View>
   );
 }

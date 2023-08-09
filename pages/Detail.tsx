9c2cbad913bfd24e4d6ccd97d975b5f22bc88bcc
katayama8000 <tattu.0310@gmail.com>
2023-08-09 19:36:18 +0900

    delete

--- pages/Detail.tsx
+++ pages/Detail.tsx
@@ -1,18 +1,12 @@
 import { Text, View, Button } from 'react-native';
 import type { RouteProp } from '@react-navigation/native';
-import { useEffect } from 'react';
 
 type RootStackParamList = {
   Home: undefined;
-  Details: { itemId: number; otherParam: string };
+  Details: { itemId: number; otherParam: string; age: number };
 };
 
 type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
-
-// type DetailsScreenNavigationProp = NativeStackNavigationProp<
-//   RootStackParamList,
-//   'Details'
-// >;
 
 type Props = {
   route: DetailsScreenRouteProp;
@@ -20,28 +14,19 @@ type Props = {
 };
 
 export function DetailsScreen({ route, navigation }: Props) {
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     console.log('useFocusEffect');
-  //     return () => console.log('unmounting...');
-  //   }, [])
-  // );
-  // useEffect(() => {
-  //   console.log('useEffect');
-  //   return () => console.log('unmounting...');
-  // }, []);
-  console.log('re-render DetailsScreen');
-  const { itemId, otherParam } = route.params;
+  const { itemId, otherParam, age } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
       <Text>itemId: {JSON.stringify(itemId)}</Text>
       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Text>age: {JSON.stringify(age)}</Text>
       <Button
         title="Go to Details... again"
         onPress={() =>
-          navigation.push('Details', {
+          navigation.navigate('Details', {
             itemId: Math.floor(Math.random() * 100),
+            otherParam: 'I am japanese',
+            age: 20,
           })
         }
       />
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,44 +1,9 @@
-import { Link, useFocusEffect } from '@react-navigation/native';
-import { Text, View, Button } from 'react-native';
-import * as Linking from 'expo-linking';
-import * as Notifications from 'expo-notifications';
-import { useCallback, useEffect, useState } from 'react';
+import { Link } from '@react-navigation/native';
+import { View, Button } from 'react-native';
 
 export function HomeScreen({ navigation }) {
-  // const func = (async () => {
-  //   const str = await Linking.getInitialURL();
-  //   console.log(str);
-  //   return str;
-  // })();
-  const [on, setOn] = useState<boolean>(true);
-
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     console.log('useFocusEffect');
-  //     return () => console.log('unmount!!!');
-  //   }, [])
-  // );
-
-  // useEffect(() => {
-  //   console.log('useEffect');
-  //   return () => console.log('unmount!!!');
-  // }, []);
-  // const urlHooks = Linking.useURL();
-  // console.log('urlHooks', urlHooks);
-
-  useEffect(() => {
-    const listener = navigation.addListener('focus', () => {
-      checkState();
-    });
-    return listener;
-  }, [on]);
-
-  const checkState = async () => {
-    console.log('on', on);
-  };
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen </Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
@@ -47,22 +12,9 @@ export function HomeScreen({ navigation }) {
         title="Go to Notification"
         onPress={() => navigation.navigate('Notification')}
       />
-      {/* <Button
-        title="Linking expo"
-        onPress={() => Linking.openURL('https://expo.dev')}
-      /> */}
       <Link to="/list" style={{ color: 'red' }}>
         Link
       </Link>
-      <Text>{on ? 'on' : 'off'}</Text>
-      <Button
-        title="on"
-        onPress={() => {
-          setOn(!on);
-        }}
-      />
-      <Button title="check" onPress={checkState} />
-
       {/* <Button
         title="check"
         onPress={async () => {
